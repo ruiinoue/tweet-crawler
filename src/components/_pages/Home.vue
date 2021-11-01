52133e4bf6977b411cc99a014b1b8f5ddd083fb4
ruiinoue <jingshangrui422@gmail.com>
2021-11-01 20:44:07 +0900

    [add]データテーブルの雛形を作成

--- src/components/_pages/Home.vue
+++ src/components/_pages/Home.vue
@@ -1,5 +1,40 @@
 <template>
-  <div>{{ userTimeline }}</div>
+  <div class="container mx-auto px-4 sm:px-8">
+    <div class="py-8">
+      <div class="datatable-container">
+        <div class="datatable-inner">
+          <table class="datatable">
+            <thead>
+              <tr>
+                <th class="datatable-thead-tr-th">投稿日</th>
+                <th class="datatable-thead-tr-th">RT数</th>
+                <th class="datatable-thead-tr-th">いいね数</th>
+                <th class="datatable-thead-tr-th">URL</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td class="datatable-tbody-tr-td">2021-11-01 12:23:30</td>
+                <td class="datatable-tbody-tr-td">
+                  <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                    <span aria-hidden class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+                    <span class="relative">23</span>
+                  </span>
+                </td>
+                <td class="datatable-tbody-tr-td">
+                  <span class="relative inline-block px-3 py-1 font-semibold text-red-900 leading-tight">
+                    <span aria-hidden class="absolute inset-0 bg-red-200 opacity-50 rounded-full"></span>
+                    <span class="relative">85</span>
+                  </span>
+                </td>
+                <td class="datatable-tbody-tr-td">https://twitter.com</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 import VueAxios from "vue-axios";
+import "./index.css";
 
 createApp(App).use(router).use(VueAxios, axios).mount("#app");
