03d8f327301b12f1895cfaccbd2b15a070a8afc9
ruiinoue <jingshangrui422@gmail.com>
2021-11-03 14:39:18 +0900

    [add]フォームのコンポーネント

---
+++ src/components/_atoms/form/FormInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <label class="block">
+    <span class="text-gray-700">{{ label }}</span>
+    <input
+      :value="modelValue"
+      type="text"
+      :name="name"
+      class="form-input"
+      :placeholder="placeholder"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </label>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    modelValue: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+
+  emits: ["update:modelValue"],
+});
+</script>
--- src/components/_organisms/SearchUserTimeline.vue
+++ src/components/_organisms/SearchUserTimeline.vue
@@ -1,27 +1,24 @@
 <template>
   <div>
-    <label class="block">
-      <span class="text-gray-700">ユーザー名</span>
-      <input
-        type="text"
-        name="screenName"
-        class="form-input"
-        placeholder="例）inouuuuuuuu_00"
-      />
-    </label>
+    <FormInput
+      v-model:modelValue="screenName"
+      :label="'ユーザー名'"
+      :name="'screenName'"
+      :placeholder="'例）inouuuuuuuu_00'"
+    />
 
     <div class="block mt-4">
       <span class="text-gray-700">RTを含めるか</span>
       <div class="mt-2 flex flex-row">
         <div class="mr-8">
           <label class="inline-flex items-center">
-            <input type="radio" name="includeRts" value="1" checked />
+            <input v-model="includeRts" type="radio" name="includeRts" :value="false" checked />
             <span class="ml-2">含める</span>
           </label>
         </div>
         <div>
           <label class="inline-flex items-center">
-            <input type="radio" name="includeRts" value="2" />
+            <input v-model="includeRts" type="radio" name="includeRts" :value="true" />
             <span class="ml-2">含めない</span>
           </label>
         </div>
@@ -33,13 +30,13 @@
       <div class="mt-2 flex flex-row">
         <div class="mr-8">
           <label class="inline-flex items-center">
-            <input type="radio" name="excludeReplies" value="1" checked />
+            <input v-model="excludeReplies" type="radio" name="excludeReplies" :value="false" checked />
             <span class="ml-2">除外する</span>
           </label>
         </div>
         <div>
           <label class="inline-flex items-center">
-            <input type="radio" name="excludeReplies" value="2" />
+            <input v-model="excludeReplies" type="radio" name="excludeReplies" :value="true" />
             <span class="ml-2">除外しない</span>
           </label>
         </div>
@@ -47,13 +44,38 @@
     </div>
 
     <div class="mt-4">
-      <button class="btn btn-blue">検索する</button>
+      <button class="btn btn-blue" @click="searchUserTimeline">検索する</button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
-export default defineComponent({});
+import FormInput from "@/components/_atoms/form/FormInput.vue";
+
+export default defineComponent({
+  components: {
+    FormInput,
+  },
+
+  setup() {
+    const screenName = ref<string>("");
+    const includeRts = ref<boolean>(false);
+    const excludeReplies = ref<boolean>(false);
+
+    const searchUserTimeline = () => {
+      console.log(screenName.value);
+      console.log(includeRts.value);
+      console.log(excludeReplies.value);
+    };
+
+    return {
+      screenName,
+      includeRts,
+      excludeReplies,
+      searchUserTimeline,
+    };
+  },
+});
 </script>
